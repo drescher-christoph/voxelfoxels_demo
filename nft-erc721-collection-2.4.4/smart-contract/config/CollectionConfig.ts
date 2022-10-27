f8a1a4fc827fbd45dc61ee19803d169e94876137
christoph_drescher <cdrescher03@gmail.com>
2022-10-27 09:57:24 +0200

    First Commit - Public Sale is Open - Collection revealed

---
+++ nft-erc721-collection-2.4.4/smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.ethereumTestnet,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'VoxelFoxels',
+  tokenName: 'VoxelFoxels Genesis',
+  tokenSymbol: 'VXFX',
+  hiddenMetadataUri: 'ipfs://QmXpPYq5FV529UejBxQdNJCTsstP5vvKWfe2BFesnffPnC/hidden.json',
+  maxSupply: 30,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0x7AD41bCF6c50e3e939A70925877DD9407f3F30F3",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
